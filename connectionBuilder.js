b6ae68e75fd9c9d1b5994f00695c2ad724b9ed39
John Kew <john.v.kew.ii@gmail.com>
2019-04-27 23:28:57 -0700

    Add initial skeleton of plugin files

---
+++ connectionBuilder.js
@@ -0,0 +1,4 @@
+(function dsbuilder(attr) {
+    var urlBuilder = "jdbc:airtable:https://api.airtable.com/v0"
+    return [urlBuilder];
+})
---
+++ connectionProperties.js
@@ -0,0 +1,16 @@
+(function propertiesBuilder(attr) {
+    //This script is only needed if you are using a JDBC driver.
+    var params = {};
+
+    // Set keys for properties needed for connecting using JDBC.
+    // Set connection properties from existing attributes.
+    params["key"] = attr[connectionHelper.attributePassword];
+    params["base"] = attr[connectionHelper.attributeDatabase];
+    params["table"] = "Breeding%20Plants";
+
+    var formattedParams = [];
+    for (var key in params) {
+        formattedParams.push(connectionHelper.formatKeyValuePair(key, params[key]));
+    }
+    return formattedParams;
+})
